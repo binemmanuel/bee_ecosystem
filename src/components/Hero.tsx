dc9d1677a28facffa83156c8226f13965559e86b
Bin Emmanuel <textme@binemmanuel.com>
2026-02-16 14:37:11 +0100

    Refactor Hero component to improve layout structure

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,11 @@
 import HeadingAndTagline from "./HeadingAndTagline";
-import HeroActions from "./HeroActions";
 import HeroBackground from "./HeroBackground";
+import HeroActions from "./HeroActions";
 import NowLivePile from "./NowLivePile";
 
 export default function Hero() {
 	return (
-		<div className="h-[90vh] text-white flex flex-1 items-center justify-center flex-col gap-8.75">
+		<div className="relative h-[90vh] text-white flex flex-1 items-center justify-center flex-col gap-8.75">
 			<HeroBackground />
 
 			<NowLivePile />
