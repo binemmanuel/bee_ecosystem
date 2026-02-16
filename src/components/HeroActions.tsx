fde1e4d5b297a19a4266191b193060f5b3034065
Bin Emmanuel <textme@binemmanuel.com>
2026-02-16 16:23:35 +0100

    Refactor HeroActions component to use ElevatedButton for improved button management

--- src/components/HeroActions.tsx
+++ src/components/HeroActions.tsx
@@ -1,35 +1,14 @@
-import Image from "next/image";
+import ElevatedButton from "./buttons/ElevatedButton";
+import OutlinedButton from "./buttons/OutlinedButton";
 
 export default function HeroActions() {
 	return (
 		<div className="flex flex-col md:flex-row gap-4 md:gap-5 mt-8 md:mt-12.5 justify-center items-center w-full px-4 md:px-0">
-			<button
-				type="button"
-				className="w-full md:w-auto px-4.5 py-3 bg-[#EDA909] text-[15px] font-semibold rounded-[10px] text-black flex justify-center items-center gap-2.5"
-			>
-				Join Presale
-				<Image
-					alt="arrow right"
-					src="/arrow-narrow-right.svg"
-					height={18}
-					width={18}
-				/>
-			</button>
+			<ElevatedButton label="Join Presale" />
 
 			<OutlinedButton label="Explore Marketplace" />
 
 			<OutlinedButton label="Participate in DAO" />
 		</div>
-	);
-}
-
-function OutlinedButton({ label }: { label: string }) {
-	return (
-		<button
-			type="button"
-			className="w-full md:w-auto px-4.5 py-3  text-[15px] font-semibold rounded-[10px] border border-white text-white"
-		>
-			{label}
-		</button>
 	);
 }
