0fbd762e6267148f09cdc1ce141295c9bcf264c5
Bin Emmanuel <textme@binemmanuel.com>
2026-02-16 20:26:50 +0100

    Refactor ElevatedButton and OutlinedButton components to use children prop for improved flexibility

--- src/components/buttons/ElevatedButton.tsx
+++ src/components/buttons/ElevatedButton.tsx
@@ -1,18 +1,16 @@
-import Image from "next/image";
-
-export default function ElevatedButton({ label }: { label: string }) {
+export default function ElevatedButton({
+	children,
+	className,
+}: {
+	children: React.ReactNode;
+	className?: string;
+}) {
 	return (
 		<button
 			type="button"
-			className="w-full md:w-auto px-4.5 py-3 bg-[#EDA909] text-[15px] font-semibold rounded-[10px] text-black flex justify-center items-center gap-2.5"
+			className={`w-full md:w-auto px-4.5 py-3 bg-[#EDA909] text-[15px] font-semibold rounded-[10px] text-black flex justify-center items-center gap-2.5 ${className && className}`}
 		>
-			{label}
-			<Image
-				alt="arrow right"
-				src="/arrow-narrow-right.svg"
-				height={18}
-				width={18}
-			/>
+			{children}
 		</button>
 	);
 }
--- src/components/buttons/OutlinedButton.tsx
+++ src/components/buttons/OutlinedButton.tsx
@@ -1,10 +1,14 @@
-export default function OutlinedButton({ label }: { label: string }) {
+export default function OutlinedButton({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
 	return (
 		<button
 			type="button"
 			className="w-full md:w-auto px-4.5 py-3  text-[15px] font-semibold rounded-[10px] border border-white text-white"
 		>
-			{label}
+			{children}
 		</button>
 	);
 }
