1de2163a2bce4cfbf62e7d2100ca6ce58fa7a8d7
Bin Emmanuel <textme@binemmanuel.com>
2026-02-16 14:37:01 +0100

    Refactor homepage layout to include Stats component and adjust main structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
+import PillarsSection from "@/components/PillarsSection";
 import Navbar from "@/components/Navbar";
+import Stats from "@/components/Stats";
 import Hero from "@/components/Hero";
 
 export default function Home() {
 	return (
-		<main className=" relative">
+		<main>
 			<Navbar />
-
 			<Hero />
+			<Stats />
+			<PillarsSection />
 		</main>
 	);
 }
