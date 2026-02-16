e3b5808e44c4b6aeb4622fe9cda5ceb17e3ee0f1
Bin Emmanuel <textme@binemmanuel.com>
2026-02-16 20:25:35 +0100

    Refactor Home component layout to include header and improve structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import CommunitySection from "@/components/CommunitySection";
 import PillarsSection from "@/components/PillarsSection";
 import GovernanceCTA from "@/components/GovernanceCTA";
 import Navbar from "@/components/Navbar";
@@ -6,12 +7,18 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
 	return (
-		<main>
-			<Navbar />
-			<Hero />
-			<Stats />
-			<PillarsSection />
-			<GovernanceCTA />
-		</main>
+		<>
+			<header>
+				<Navbar />
+				<Hero />
+				<Stats />
+			</header>
+
+			<main>
+				<PillarsSection />
+				<GovernanceCTA />
+				<CommunitySection />
+			</main>
+		</>
 	);
 }
