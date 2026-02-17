01e77eee6ff1232d90f9ce0ae2c3aa41ad26b36c
Bin Emmanuel <textme@binemmanuel.com>
2026-02-17 01:13:04 +0100

    Add cursor-pointer class to ElevatedButton for better UX

--- src/components/buttons/ElevatedButton.tsx
+++ src/components/buttons/ElevatedButton.tsx
@@ -8,7 +8,7 @@ export default function ElevatedButton({
 	return (
 		<button
 			type="button"
-			className={`w-full md:w-auto px-4.5 py-3 bg-[#EDA909] text-[15px] font-semibold rounded-[10px] text-black flex justify-center items-center gap-2.5 ${className && className}`}
+			className={`w-full md:w-auto px-4.5 py-3 bg-[#EDA909] text-[15px] font-semibold rounded-[10px] text-black flex justify-center items-center gap-2.5 cursor-pointer ${className && className}`}
 		>
 			{children}
 		</button>
